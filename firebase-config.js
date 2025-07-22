e155ade96bd13b5318a85297f133ef608a777e06
Srinivasa Rao V <67523132+srinivasa150893@users.noreply.github.com>
2025-07-22 20:03:27 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,1 @@
-// firebase-config.js
 
-const firebaseConfig = {
-      apiKey: "AIzaSyAvlDtLbOYEpAb2Q3Xv_2IGaEzGdk7ZD48",
-      authDomain: "purchasing-power-app.firebaseapp.com",
-      projectId: "purchasing-power-app",
-      storageBucket: "purchasing-power-app.firebasestorage.app",
-      messagingSenderId: "662635318445",
-      appId: "1:662635318445:web:d38e2ad791f52d786f0a24",
-      measurementId: "G-38LJM9JDJT"
-    };
-
-// Export the configuration so it can be imported by other modules
-export { firebaseConfig };
-
-//
